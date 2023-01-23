8fd772e165d201183d9621dd81fac96414096f6e
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-01-24 01:05:07 +0200

    Updated build.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,10 @@ publishing {
         val githubUsername = project.findProperty("github.auth.username") ?: ""
         val githubPassword = project.findProperty("github.auth.password") ?: ""
 
+        val selfURL = project.findProperty("self.url") ?: ""
+        val selfUsername = project.findProperty("self.auth.username") ?: ""
+        val selfPassword = project.findProperty("self.auth.password") ?: ""
+
         maven(url = gitlabURL as String) {
             name = "gitlab"
             credentials(HttpHeaderCredentials::class) {
@@ -81,6 +85,14 @@ publishing {
                 password = githubPassword as String
             }
         }
+
+        maven(url = selfURL as String) {
+            name = "self"
+            credentials(PasswordCredentials::class) {
+                username = selfUsername as String
+                password = selfPassword as String
+            }
+        }
     }
 }
 
@@ -92,4 +104,9 @@ tasks.register("publishGitLab") {
 tasks.register("publishGitHub") {
     dependsOn("publishMavenPublicationToGithubRepository")
     description = "Publishes to GitHub"
+}
+
+tasks.register("publishSelf") {
+    dependsOn("publishMavenPublicationToSelfRepository")
+    description = "Publishes to Self hosted repository"
 }
