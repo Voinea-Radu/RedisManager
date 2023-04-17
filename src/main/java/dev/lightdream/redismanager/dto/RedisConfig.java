16fc04947b72cc697eb1bc8393d506d10f990fe2
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-17 22:21:46 +0300

    Removed unnecessary expose annotations

--- src/main/java/dev/lightdream/redismanager/dto/RedisConfig.java
+++ src/main/java/dev/lightdream/redismanager/dto/RedisConfig.java
@@ -1,6 +1,5 @@
 package dev.lightdream.redismanager.dto;
 
-import com.google.gson.annotations.Expose;
 import lombok.Getter;
 
 import java.util.UUID;
@@ -9,15 +8,15 @@ import java.util.UUID;
 @SuppressWarnings("FieldMayBeFinal")
 public class RedisConfig {
 
-    private @Expose String host = "127.0.0.1";
-    private @Expose int port = 6379;
-    private @Expose String password = "password";
-    private @Expose String channel = "channel";
+    private String host = "127.0.0.1";
+    private int port = 6379;
+    private String password = "password";
+    private String channel = "channel";
 
     // Advanced settings
-    private @Expose String redisID = UUID.randomUUID().toString();
-    private @Expose int timeout = 2000; // 2s
-    private @Expose int waitBeforeIteration = 50; // 50ms
+    private String redisID = UUID.randomUUID().toString();
+    private int timeout = 2000; // 2s
+    private int waitBeforeIteration = 50; // 50ms
 
 }
 
